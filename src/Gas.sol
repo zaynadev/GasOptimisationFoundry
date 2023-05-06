ef56f580dc65e6f099bc55aa2542ab9152cbc095
Carson Halter <84420466+cah311@users.noreply.github.com>
2023-05-05 21:02:25 -0600

    Reorganized variables in Payment Struct
    Changed the ordering of the amount and paymentType variables to
    reduce a storage slot. This also reduced the deployment cost
    and average cost of the transfer and whiteTransfer functions

--- src/Gas.sol
+++ src/Gas.sol
@@ -33,13 +33,14 @@ contract GasContract is Ownable, Constants {
     History[] public paymentHistory; // when a payment was updated
 
     struct Payment {
-        PaymentType paymentType;
+        uint256 amount;
         uint256 paymentID;
         bool adminUpdated;
+        PaymentType paymentType;
         string recipientName; // max 8 characters
         address recipient;
         address admin; // administrators address
-        uint256 amount;
+        
     }
 
     struct History {
